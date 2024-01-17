5dfc9fcde3e9dd72032af473e5e5cb430f11a83c
Brecht Vandevoort <brechtvandevoort@gmail.com>
2024-01-17 17:39:41 +0100

    removing old example file

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 from utils.screen_load import create_load_screen
 from utils.screen_visualize import create_visualize_screen
 
-from utils.request import Request
-
 st.title("Schema Matching for Health Data using Foundation Models")
 st.write("This will be an actual demo some day.")
 
