fbbb52b6fc90283e14a573ca52f5373ec9162ad4
Brecht Vandevoort <brechtvandevoort@gmail.com>
2024-01-23 15:44:27 +0100

    compare_to now implemented for graph visualization and voting details

--- utils/screen_visualize.py
+++ utils/screen_visualize.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 import streamlit as st
 from st_cytoscape import cytoscape
 from streamlit_extras.stylable_container import stylable_container
@@ -9,6 +9,10 @@ COLOR_YES = "#4bff4b"
 COLOR_NO = "#ff4b4b"
 COLOR_UNKNOWN = "#bfbfbf"
 
+COLOR_YES_2 = "#2d592d"
+COLOR_NO_2 = "#692424"
+COLOR_UNKNOWN_2 = "#4a4a4a"
+
 def create_visualize_screen(mss: ModelSessionState):
     st.header("Results")
 
@@ -16,29 +20,51 @@ def create_visualize_screen(mss: ModelSessionState):
         st.warning("No results to visualize yet")
         return
 
+    result = mss.result
+    compare_to = mss.compare_to
+
     # Selectors for visualized vote types
     #st.write("Show votes:")
-    cols = st.columns(6)
+    cols = st.columns(3)
     with cols[0]:
         with stylable_container(
             key="yes_button",
             css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_YES};border-color: {COLOR_YES};}}',
         ):
-            show_yes = st.checkbox("Show yes-votes", value=True, key="vote_yes_checkbox")
+            show_yes = st.checkbox(f"Show yes-votes for {result.name}", value=True, key="vote_yes_checkbox")
     with cols[1]:
         with stylable_container(
             key="no_button",
             css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_NO};border-color: {COLOR_NO};}}',
         ):
-            show_no = st.checkbox("Show no-votes", value=True, key="vote_no_checkbox")
+            show_no = st.checkbox(f"Show no-votes for {result.name}", value=True, key="vote_no_checkbox")
     with cols[2]:
         with stylable_container(
             key="unknown_button",
             css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_UNKNOWN};border-color: {COLOR_UNKNOWN};}}',
         ):
-            show_unknown = st.checkbox("Show unknown-votes", value=True, key="vote_unknown_checkbox")
-
-    result = mss.result
+            show_unknown = st.checkbox(f"Show unknown-votes for {result.name}", value=True, key="vote_unknown_checkbox")
+        
+    show_yes_ct = show_no_ct = show_unknown_ct = False
+    if compare_to is not None:
+        with cols[0]:
+            with stylable_container(
+                key="yes_button_2",
+                css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_YES_2};border-color: {COLOR_YES_2};}}',
+            ):
+                show_yes_ct = st.checkbox(f"Show yes-votes for {compare_to.name}", value=True, key="vote_yes_checkbox_2")
+        with cols[1]:
+            with stylable_container(
+                key="no_button_2",
+                css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_NO_2};border-color: {COLOR_NO_2};}}',
+            ):
+                show_no_ct = st.checkbox(f"Show no-votes for {compare_to.name}", value=True, key="vote_no_checkbox_2")
+        with cols[2]:
+            with stylable_container(
+                key="unknown_button_2",
+                css_styles=f'span:has(+ input[aria-checked="true"]) {{background-color: {COLOR_UNKNOWN_2};border-color: {COLOR_UNKNOWN_2};}}',
+            ):
+                show_unknown_ct = st.checkbox(f"Show unknown-votes for {compare_to.name}", value=True, key="vote_unknown_checkbox_2")
 
     # Quickly verify that all attributes have a uid
     for attr in result.parameters.source_relation.attributes:
@@ -47,7 +73,7 @@ def create_visualize_screen(mss: ModelSessionState):
             return
     for attr in result.parameters.target_relation.attributes:
         if attr.uid is None:
-            st.error(f"Attribute  {attr.name} uid is None")
+            st.error(f"Attribute {attr.name} uid is None")
             return
 
     # Lookup tables before constructing the graph
@@ -68,60 +94,9 @@ def create_visualize_screen(mss: ModelSessionState):
     ]
 
     # Add edges (=votes) to the graph
-    for pair, result_pair in result.pairs.items():
-        source_uid = pair.source.uid
-        target_uid = pair.target.uid
-        if source_uid not in left_attr_lookup:
-            st.error(f"Source uid {source_uid} not found in left_attr_lookup")
-            return
-        if target_uid not in right_attr_lookup:
-            st.error(f"Target uid {target_uid} not found in right_attr_lookup")
-            return
-        source_name = left_attr_lookup[source_uid].name
-        target_name = right_attr_lookup[target_uid].name
-        num_yes = len([decision for decision in result_pair.votes if decision.vote == Vote.YES])
-        num_no = len([decision for decision in result_pair.votes if decision.vote == Vote.NO])
-        num_unknown = len([decision for decision in result_pair.votes if decision.vote == Vote.UNKNOWN])
-        if show_yes:
-            elements.append(
-                {
-                    "data": {
-                        "source": str(source_uid),
-                        "target": str(target_uid),
-                        "id": f"{source_name}➞{target_name}-yes",
-                        "weight": num_yes,
-                        "color": COLOR_YES
-                    },
-                    "selectable": False,
-                }
-            )
-        if show_no:
-            elements.append(
-                {
-                    "data": {
-                        "source": str(source_uid),
-                        "target": str(target_uid),
-                        "id": f"{source_name}➞{target_name}-no",
-                        "weight": num_no,
-                        "color": COLOR_NO
-                    },
-                    "selectable": False,
-                }
-            )
-        if show_unknown:
-            elements.append(
-                {
-                    "data": {
-                        "source": str(source_uid),
-                        "target": str(target_uid),
-                        "id": f"{source_name}➞{target_name}-unknown",
-                        "weight": num_unknown,
-                        "color": COLOR_UNKNOWN
-                    },
-                    "selectable": False,
-                }
-            )
-
+    elements.extend(_create_edge_elements(result, left_attr_lookup, right_attr_lookup, show_yes, show_no, show_unknown, COLOR_YES, COLOR_NO, COLOR_UNKNOWN, "result"))
+    if compare_to is not None:
+        elements.extend(_create_edge_elements(compare_to, left_attr_lookup, right_attr_lookup, show_yes_ct, show_no_ct, show_unknown_ct, COLOR_YES_2, COLOR_NO_2, COLOR_UNKNOWN_2, "compare_to"))
 
 
     stylesheet = [
@@ -177,12 +152,12 @@ def create_visualize_screen(mss: ModelSessionState):
     # If exactly 1 source and target attribute selected: show more info on this pair
     if len(selected_source) == 1 and len(selected_target) == 1:
         attr_pair = AttributePair(selected_source[0], selected_target[0])
-        _voting_details(result, attr_pair)
+        _voting_details(result, attr_pair, compare_to)
     else:
         st.info("Select an attribute pair in the graph to see voting details for the selected pair")
 
 
-def _voting_details(result: Result, attr_pair: AttributePair):
+def _voting_details(result: Result, attr_pair: AttributePair, compare_to: Optional[Result] = None):
     """Display the voting details for a given attribute pair."""
     st.header(f"Voting details between `{result.parameters.source_relation.name}.{attr_pair.source.name}` and `{result.parameters.target_relation.name}.{attr_pair.target.name}`")
 
@@ -190,12 +165,85 @@ def _voting_details(result: Result, attr_pair: AttributePair):
         st.error("Selected attribute pair not found in result. This is most likely a bug.")
         return
 
-    # Show votes
-    votes = result.pairs[attr_pair].votes
-    for i, vote in enumerate(votes):
-        with st.expander(f"Vote {i+1}: {vote.vote.name}"):
-            st.text(vote.explanation)
+    if compare_to is not None and attr_pair not in compare_to.pairs:
+        st.error("Selected attribute pair not found in compare_to. This is most likely a bug.")
+        return
 
+    # Show votes
+    num_cols = 1 if compare_to is None else 2
+    cols = st.columns(num_cols)
+    with cols[0]:
+        if compare_to is not None:
+            st.subheader(f"Votes for {result.name}")
+        votes = result.pairs[attr_pair].votes
+        for i, vote in enumerate(votes):
+            with st.expander(f"Vote {i+1}: {vote.vote.name}"):
+                st.text(vote.explanation)
+    if compare_to is not None:
+        with cols[1]:
+            st.subheader(f"Votes for {compare_to.name}")
+            votes = compare_to.pairs[attr_pair].votes
+            for i, vote in enumerate(votes):
+                with st.expander(f"Vote {i+1}: {vote.vote.name}"):
+                    st.text(vote.explanation)
+
+
+def _create_edge_elements(result: Result, left_attr_lookup, right_attr_lookup, show_yes, show_no, show_unknown, color_yes, color_no, color_unknown, id_prefix: str) -> list[dict[str, Any]]:
+    elements = []
+    for pair, result_pair in result.pairs.items():
+        source_uid = pair.source.uid
+        target_uid = pair.target.uid
+        if source_uid not in left_attr_lookup:
+            st.error(f"Source uid {source_uid} not found in left_attr_lookup")
+            raise ValueError()
+        if target_uid not in right_attr_lookup:
+            st.error(f"Target uid {target_uid} not found in right_attr_lookup")
+            raise ValueError()
+        source_name = left_attr_lookup[source_uid].name
+        target_name = right_attr_lookup[target_uid].name
+        num_yes = len([decision for decision in result_pair.votes if decision.vote == Vote.YES])
+        num_no = len([decision for decision in result_pair.votes if decision.vote == Vote.NO])
+        num_unknown = len([decision for decision in result_pair.votes if decision.vote == Vote.UNKNOWN])
+        if show_yes:
+            elements.append(
+                {
+                    "data": {
+                        "source": str(source_uid),
+                        "target": str(target_uid),
+                        "id": f"{id_prefix}.{source_name}➞{target_name}-yes",
+                        "weight": num_yes,
+                        "color": color_yes
+                    },
+                    "selectable": False,
+                }
+            )
+        if show_no:
+            elements.append(
+                {
+                    "data": {
+                        "source": str(source_uid),
+                        "target": str(target_uid),
+                        "id": f"{id_prefix}.{source_name}➞{target_name}-no",
+                        "weight": num_no,
+                        "color": color_no
+                    },
+                    "selectable": False,
+                }
+            )
+        if show_unknown:
+            elements.append(
+                {
+                    "data": {
+                        "source": str(source_uid),
+                        "target": str(target_uid),
+                        "id": f"{id_prefix}.{source_name}➞{target_name}-unknown",
+                        "weight": num_unknown,
+                        "color": color_unknown
+                    },
+                    "selectable": False,
+                }
+            )
+    return elements
 
 def toy_example():
     #TODO: remove when example no longer needed
