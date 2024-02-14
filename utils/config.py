e482eabd072100de40de29a6f7ce04570427c40b
Marcel Parciak <marcel.parciak@gmail.com>
2024-02-14 09:35:21 +0100

    fixed missing change in resources after refactoring of prompt building
    Signed-off-by: Marcel Parciak <marcel.parciak@gmail.com>

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,12 @@
 import os
 
 default_config = {
-    "QUERY_OPENAI": True,  # if set to True, this generates a random result instead of prompting the LLM
+    "QUERY_OPENAI": True,  # if set to False, this generates a random result instead of prompting the LLM
     "OPENAI_MODEL": "gpt-3.5-turbo-1106",  # the model to use on the OpenAI API
     "OPENAI_N": 3,  # the number of answers to generate per prompt
     "OPENAI_TEMPERATURE": 1.0,  # the model temperature to use
     "OPENAI_TIMEOUT": 60,  # the timeout for OpenAI API calls
+    "TEMPLATE_DIR": "resources/prompt_templates",  # the directory where prompt templates are stored
     "PARALLEL_OPENAI_REQUESTS": 5,  # the maximum number of parallel requests that will be sent to the OpenAI API (lower this to fix frequent RateLimitErrors)
     "SQLITE_PATH": "dev.sqlite3",  # the path to the SQLite database file. Set this to None to disable storage.
 }
